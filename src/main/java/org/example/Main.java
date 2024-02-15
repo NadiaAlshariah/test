733cf2914dd7720513957752230a567f63b26219
NadiaAlshariah <nadiakalshariah@gmail.com>
2024-02-15 13:35:55 +0300

    install fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.neo4j.driver.*;
+import org.neo4j.driver.AuthTokens;
 import picocli.CommandLine;
 
 public class Main
@@ -8,14 +9,17 @@ public class Main
     public static void main( String[] args )
     {
         try (Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"))) {
-
+            System.out.println("driver is here");
         try (Session session = driver.session()) {
+            System.out.println("session is here");
             try (Transaction tx = session.beginTransaction()) {
+                System.out.println("transaction is here");
                 Result result = tx.run("CREATE (n:Person {name: 'John Doe'}) RETURN n.name");
                 tx.commit();
             }
         }
     }
-        CommandLine.run(new MainCommand(), args);
+        int exitCode = new CommandLine(new MainCommand()).execute(args);
+        System.exit(exitCode);
     }
 }
